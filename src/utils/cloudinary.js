64484f2721e3389ebf19842225e3d1ca1c91fb07
AryanKadam1134 <aryankadam3114@gmail.com>
2025-12-14 23:26:57 +0530

    File upload utility

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,37 @@
+import { v2 as cloudinary } from "cloudinary";
+
+// To Read, Write, Delete file (Manaege File)
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET_KEY, // Click 'View API Keys' above to copy your API secret
+});
+
+// This is a utility to upload any kind of file to cloudinary
+const uploadToCloudinary = async (localFilePath) => {
+  if (!localFilePath) return; // Check of the local path exist or not
+
+  try {
+    const uploadedResult = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    console.log(
+      "File has uploaded to cloudinary successfully: ",
+      uploadedResult.url // Print the url
+    );
+
+    return uploadedResult; // Retirn the result
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the file form the server
+
+    console.log("Error uplaoding the file to cloudinary: ", error);
+
+    return null;
+  }
+};
+
+export default uploadToCloudinary;
