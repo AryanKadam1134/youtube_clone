fa6039c25b235cbab30898653671c2f2ac6c9b19
AryanKadam1134 <aryankadam3114@gmail.com>
2025-12-16 22:13:05 +0530

    wrote routes nad controllers

--- src/app.js
+++ src/app.js
@@ -20,4 +20,12 @@ app.use(express.static("public"));
 
 app.use(cookieParser());
 
+// import routes
+import userRouter from "./routes/user.routes.js";
+
+// use routes
+app.use("/api/v1/users", userRouter);
+
+// http://localhost:8000/api/v1/users/register
+
 export default app;
